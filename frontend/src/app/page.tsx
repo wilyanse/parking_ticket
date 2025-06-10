7268855e1bf5807fc2e88d89da0e7e874c4a6145
wilyanse <willjose51@gmail.com>
2025-06-10 13:27:27 +0800

    devops(docker compose): implement development docker compose file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our ssss
           </a>
         </div>
       </main>
